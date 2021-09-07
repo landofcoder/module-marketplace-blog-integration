e31c7ee8c0410585bfb367d3321a88f51a28b97c
landofcoder <landofcoder@gmail.com>
2021-09-07 17:46:57 +0700

    added module files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Magento 2 Seller Blog for Marketplace
+
+The module is guide how to integration 3th party extensions into our Marketplace Solution
+
+## Features
+- Compatible Magefan_Blog for Marketplace
+- Allow seller manage blog posts
+- Allow seller manage blog categories
+- Show seller blogs on frontend
+- Show latest blog posts, blog categories, recent reviews block on seller detail page on frontend.
---
+++ Setup/InstallSchema.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Landofcoder
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Landofcoder.com license that is
+ * available through the world-wide-web at this URL:
+ * https://landofcoder.com/terms
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category   Landofcoder
+ * @package    Lofmp_SellerBlog
+ * @copyright  Copyright (c) 2021 Landofcoder (https://www.landofcoder.com/)
+ * @license    https://landofcoder.com/terms
+ */
+namespace Lofmp\SellerBlog\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Ddl\Table;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+        $installer->getConnection()->addColumn(
+            $installer->getTable('magefan_blog_category'),
+            'seller_id',
+            [
+                'type' => Table::TYPE_INTEGER,
+                'nullable' => false,
+                'comment' => 'Seller Id'
+            ]
+        );
+
+        $installer->getConnection()->addColumn(
+            $installer->getTable('magefan_blog_comment'),
+            'seller_id',
+            [
+                'type' => Table::TYPE_INTEGER,
+                'nullable' => false,
+                'comment' => 'Seller Id'
+            ]
+        );
+
+        $installer->getConnection()->addColumn(
+            $installer->getTable('magefan_blog_post'),
+            'seller_id',
+            [
+                'type' => Table::TYPE_INTEGER,
+                'nullable' => false,
+                'comment' => 'Seller Id'
+            ]
+        );
+
+        $installer->getConnection()->addColumn(
+            $installer->getTable('magefan_blog_tag'),
+            'seller_id',
+            [
+                'type' => Table::TYPE_INTEGER,
+                'nullable' => false,
+                'comment' => 'Seller Id'
+            ]
+        );
+
+        /**
+         * Create table 'lofmp_sellerblog_setting'
+         */
+        $table = $installer->getConnection()->newTable(
+            $installer->getTable('lofmp_sellerblog_setting')
+        )->addColumn(
+            'seller_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            ['unsigned' => true, 'nullable' => false, 'primary' => true],
+            'Seller ID'
+        )->addColumn(
+            'enable',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            ['nullable' => false, 'default' => true],
+            'Enable Seller Blog'
+        )->addColumn(
+            'show_blog_categories',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            ['nullable' => false, 'default' => true],
+            'Show blog Categories'
+        )->addColumn(
+            'show_latest_posts',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            ['nullable' => false, 'default' => true],
+            'Show Latest Posts'
+        )->addColumn(
+            'show_related_posts',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            ['nullable' => false, 'default' => true],
+            'Show Related Posts in Product detail page'
+        )->addColumn(
+            'show_related_products',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            ['nullable' => false, 'default' => true],
+            'Show Related Products in Post detail page'
+        )->addForeignKey(
+            $installer->getFkName('lofmp_sellerblog_setting', 'seller_id', 'lof_marketplace_seller','seller_id'),
+            'seller_id',
+            $installer->getTable('lof_marketplace_seller'),
+            'seller_id',
+            \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+        )->setComment(
+            'Seller Blog Settings Table'
+        );
+        $installer->getConnection()->createTable($table);
+    }
+}
